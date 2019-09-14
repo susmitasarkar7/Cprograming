106f48c60a986338e9795a408fec3618020d8910
Susmita Sarkar <sarkarsusmita253@gmail,com>
2019-09-15 00:14:51 +0530

    While

---
+++ while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdio.h>
+void main()
+{
+    int x;
+    long int fact = 1;
+    printf("Enter any number to find factorial:\n"); /*read the number*/
+    scanf("%d",&x);
+    while (x > 0)
+    {
+        fact = fact * x; /* factorial calculation*/
+        x=x-1;
+    }
+    printf("Factorial is %ld",fact);
+}
